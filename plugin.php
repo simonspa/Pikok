9448945cf1245f5fb4f659f71ffb2cb1a746f864
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2013-05-13 13:44:03 +0200

    Pikok is functional

---
+++ plugin.php
@@ -0,0 +1,32 @@
+<?php
+
+class Pikok extends KokenPlugin {
+
+	function __construct()
+	{
+		$this->require_setup = true;
+		$this->register_hook('before_closing_head', 'render');
+	}
+
+	function render()
+	{
+
+		echo <<<OUT
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + '{$this->data->piwik_url}';
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", '{$this->data->piwik_siteid}']);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+OUT;
+
+	}
+}
+
