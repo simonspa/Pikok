8db62f4e42912f3cd770d15335167d5a371e9120
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2013-05-14 23:12:52 +0200

    Fixed an error

--- plugin.php
+++ plugin.php
@@ -13,17 +13,17 @@ class Pikok extends KokenPlugin {
 
 		echo <<<OUT
 <script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://{$this->data->piwik_url}/";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "{$this->data->piwik_site_id}"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-  })();
+        var _paq = _paq || [];
+        _paq.push(["trackPageView"]);
+        _paq.push(["enableLinkTracking"]);
+      
+        (function() {
+          var u=(("https:" == document.location.protocol) ? "https" : "http") + "://{$this->data->piwik_url}/";
+          _paq.push(["setTrackerUrl", u+"piwik.php"]);
+          _paq.push(["setSiteId", "{$this->data->piwik_site_id}"]);
+          var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+          g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+        })();
 </script>
 OUT;
 
